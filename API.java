c3eb8a4808fb503656b13137d380e13144ca3878
Hardik <hardikvu0204@gmail.com>
2019-07-27 12:11:02 +0530

    API for Priority Queue

---
+++ API.java
@@ -0,0 +1,22 @@
+
+public class MaxPQ<Key extends Comparable> {
+    MaxPQ(){
+    //create an empty pq
+    }
+    void insert(Key v){
+
+    }
+    Key delMax(){
+
+    }
+    boolean isEmpty(){
+
+    }
+    Key Max(){
+
+    }
+    int size(){
+
+    }
+}
+
